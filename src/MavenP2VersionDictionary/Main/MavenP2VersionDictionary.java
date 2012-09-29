083b9af7677a730b5689e3dc08a5efa15d092baf
CarolineM <cmcquatt@CarolineMs-MacBook.local>
2012-09-29 11:18:17 -0400

    starting project

---
+++ src/MavenP2VersionDictionary/Main/MavenP2VersionDictionary.java
@@ -0,0 +1,20 @@
+package MavenP2VersionDictionary.Main;
+
+public class MavenP2VersionDictionary {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MavenP2VersionDictionary dict = new MavenP2VersionDictionary();
+	}
+
+}
