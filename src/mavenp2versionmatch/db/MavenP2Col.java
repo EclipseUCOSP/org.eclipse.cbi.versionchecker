24e498dd7be146628e8871737480b510b854c175
Caroline McQuatt <cmcquatt@gmail.com>
2012-10-08 09:18:07 -0700

    added add/update/find commands

--- src/mavenp2versionmatch/db/MavenP2Col.java
+++ src/mavenp2versionmatch/db/MavenP2Col.java
@@ -1,36 +1,36 @@
 package mavenp2versionmatch.db;
 
 public enum MavenP2Col {
-	GIT_REPO("git_repo", "repo"),
-	GIT_COMMIT("git_commit", "cmt"),
-	PROJECT("project", "p"),
-	GIT_TAG("git_tag", "gtag"),
-	P2_VERSION("p2_version", "p2v"),
-	MAVEN_VERSION("maven_version", "mvnv")
+	GIT_REPO("git_repo", "-repo"),
+	GIT_COMMIT("git_commit", "-cmt"),
+	PROJECT("project", "-p"),
+	GIT_TAG("git_tag", "-gtag"),
+	P2_VERSION("p2_version", "-p2v"),
+	MAVEN_VERSION("maven_version", "-mvnv")
 	;
-	  private String col_name;
-	  private String c_arg;
-	  
-	  private MavenP2Col(String col_name, String c_arg) {
-	    this.col_name = col_name;
-	    this.c_arg = c_arg;
-	  }
-	  
-	  public String getColName() {
-	    return col_name;
-	  }
-	  
-	  private String getCommandLineArg() {
-		    return c_arg;
-		  }
-	
+	private String col_name;
+	private String c_arg;
+
+	private MavenP2Col(String col_name, String c_arg) {
+		this.col_name = col_name;
+		this.c_arg = c_arg;
+	}
+
+	public String getColName() {
+		return col_name;
+	}
+
+	private String getArgName() {
+		return c_arg;
+	}
+
 	public static MavenP2Col findByStr(String str){
-	    for (MavenP2Col e : MavenP2Col.values()) {
-	        if (e.getCommandLineArg().equals(str)) {
-	            return e;
-	        }
-	    }
-	    return null;
+		for (MavenP2Col e : MavenP2Col.values()) {
+			if (e.getArgName().equals(str)) {
+				return e;
+			}
+		}
+		return null;
 	}
 
 
--- src/mavenp2versionmatch/main/MavenP2VersionMatch.java
+++ src/mavenp2versionmatch/main/MavenP2VersionMatch.java
@@ -15,23 +15,23 @@ public class MavenP2VersionMatch {
 	 * or maven version
 	 * @param map database column names and values
 	 */
-	private static boolean isValidInput(Map<String, String> map) {
+	private static boolean isValidAdd(Map<String, String> map) {
 		return map.containsKey(MavenP2Col.GIT_REPO.getColName()) &&
 				map.containsKey(MavenP2Col.GIT_COMMIT.getColName()) &&
 				(map.containsKey(MavenP2Col.MAVEN_VERSION.getColName()) ||
 						map.containsKey(MavenP2Col.P2_VERSION.getColName()));
 	}
 	
-	/**
-	 * @param args
+	/*
+	 * get command line options starting at index 1
+	 * @param args command line input to main
+	 * @return map of db column name and input value
 	 */
-	public static void main(String[] args) {
+	private static Map<String, String> getOptions(String[] args) {
 		Map<String, String> map = new HashMap<String, String>();
 		
-		for (int i = 0; i< args.length; i++) {
-			String key = args[i].substring(1);
-			//substring for dash in commandline
-			//TODO: is there a better way?
+		for (int i = 1; i< args.length; i++) {
+			String key = args[i];
 			String val = args[++i];
 			MavenP2Col e  = MavenP2Col.findByStr(key);
 			
@@ -43,8 +43,21 @@ public class MavenP2VersionMatch {
 				map.put(e.getColName(), val);
 			}
 		}
+		//TODO: For debugging. Add to log or usr msg 
+		for (String key : map.keySet()) {
+			System.out.print(key + ": ");
+			System.out.println(map.get(key));
+		}
+		return map;
+	}
+	
+	/*
+	 * attempts to insert a record
+	 * @param map of db column name and input value
+	 */
+	private static void doAdd(Map<String, String> map) {
 			
-			if (!isValidInput(map)) {
+			if (!isValidAdd(map)) {
 				System.err.println("Invalid input. Must include git repo, " +
 						"commit and one of maven version and p2 version.");
 				System.exit(-1);
@@ -52,19 +65,63 @@ public class MavenP2VersionMatch {
 			else {
 			try {
 				SQLiteDBI dbi = new SQLiteDBI();
-				//TODO: For debugging. Add to log or usr msg 
-				for (String key : map.keySet()) {
-					System.out.print(key + ": ");
-					System.out.println(map.get(key));
-				}
 				dbi.addRecord(map);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}	
 			}
+	}
+	
+	/*
+	 * searches a record
+	 * @param map of db column name and input value
+	 */
+	private static void doFind(Map<String, String> map) {
+			//TODO: not built in dbi yet
+	}
+	
+	/*
+	 * updates a record
+	 * @param map of db column name and input value
+	 */
+	private static void doUpdate(Map<String, String> map) {
+			//TODO: not built in dbi yet
+	}
+	
+	/**
+	 * main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.err.println("Arguments not found. Must specify one of: " +
+					"add, find, update.");
+			System.exit(-1);
+		}
 		
+		Command command = Command.findByStr(args[0]);
+		if (command == null) {
+			System.err.println("Command not found: " + args[0]);
+			System.exit(-1);
+		}
+		Map<String, String> map = getOptions(args);
+		doAdd(map);
 		
+		switch (command) {
+		case ADD:
+			doAdd(map);
+			break;
+		case FIND:
+			doFind(map);
+			break;
+		case UPDATE:
+			doUpdate(map);
+			break;
+		default:
+			System.err.println("Unexpected command. Should never get here.");
+			System.exit(-1);
+		}
 	}
 
 }
