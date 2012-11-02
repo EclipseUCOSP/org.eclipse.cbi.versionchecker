71d7fd60f34ad4a673b3e368f9de0172c56a938d
Derek Ludwig <derek.s.ludwig@gmail.com>
2012-11-01 17:11:31 -0700

    Automatically detect Git repo/commit/branch and Maven version number

--- src/main/java/mvn/p2/vt/mojo/CheckVersion.java
+++ src/main/java/mvn/p2/vt/mojo/CheckVersion.java
@@ -3,9 +3,16 @@ package mvn.p2.vt.mojo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.File;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.revwalk.RevCommit;
 
 /**
  * Calls the Version Checker, prompting the user to enter a query to run.
@@ -16,33 +23,88 @@ import org.apache.maven.plugin.MojoExecutionException;
 public class CheckVersion extends AbstractMojo
 {
 	/**
-     * The query to send the version checker.
-     *
-     * @parameter expression="${callVC.query}""
-     */
-    private String query;
-	
-    public void execute() throws MojoExecutionException
-    {
-    	if (query == null || query.isEmpty()) {
-    		// prompt the user to enter a query
-    		BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); 
-    		getLog().info("Please enter your query:");
-    		try {
-				query = in.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
+	 * The query to send the version checker.
+	 *
+	 * @parameter expression="${callVC.query}""
+	 */
+	private String query;
+
+	/**
+	 * The root of the Git repository.
+	 *
+	 * @parameter default-value="${basedir}/.git"
+	 */
+	private File repositoryRoot;
+
+	/**
+	 * The name of the upstream remote repository.
+	 *
+	 * @parameter default-value="origin"
+	 */
+	private String upstreamRemote;
+
+	/**
+	 * The Maven version.
+	 *
+	 * @parameter expression="${project.version}"
+	 * @readonly
+	 */
+	private String mvnVersion;
+
+	public void execute() throws MojoExecutionException
+	{
+		String query = "add";
+		query += " -mvnv " + mvnVersion;
+
+		// Find the Git revision &c.
+		// Note: I do not call readEnvironment on the builder, since undocumented
+		// environment variables are the bane of repeatable builds.
+		try {
+			Repository repo = new FileRepositoryBuilder()
+				.setGitDir(repositoryRoot)
+				.findGitDir()
+				.build();
+
+			String branchName = repo.getBranch();
+			if (branchName == null) {
+				getLog().warn("Not on a branch");
+			} else {
+				query += " -br " + branchName;
 			}
-    	}
-    	String[] query_arr = query.split(" ");
-        getLog().info("Executing the following  query: " + query);
+
+			ObjectId head = repo.resolve("HEAD");
+			if (head == null) {
+				getLog().warn("Could not resolve HEAD");
+			} else {
+				query += " -cmt " + head.name();
+			}
+
+			Config config = repo.getConfig();
+			String url = config.getString("remote", upstreamRemote, "url");
+			if (url == null) {
+				getLog().warn("Could not find remote" + upstreamRemote);
+			} else {
+				query += " -repo " + url;
+			}
+
+		} catch (IOException ioe) {
+			getLog().error("Unable to read repository");
+			ioe.printStackTrace();
+			return;
+		} catch (IllegalArgumentException iae) {
+			getLog().error("Not a Git repository");
+			iae.printStackTrace();
+			return;
+		}
+
+		String[] query_arr = query.split(" ");
+		getLog().info("Executing the following query: " + query);
 		try {
 			// call the MvnP2Util with the given commands. This just starts the main method, set up your array of strings accordingly.
-			mavenp2versionmatch.main.MvnP2Util.main(query_arr); 
+			mavenp2versionmatch.main.MvnP2Util.main(query_arr);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}		
-    }
-
+		}
+	}
 
 }
