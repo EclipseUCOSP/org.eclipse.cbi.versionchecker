b66ebe6d262a27985485dfc7fd40c5d788cc1859
Mike Lim <mike@mjlim.net>
2012-11-03 13:42:34 -0700

    working unit test for Command class. fixed incorrect package name in CommandTest (argh)

--- src/test/java/CommandTest.java
+++ src/test/java/CommandTest.java
@@ -1,20 +1,17 @@
-package mvnp2versionmatch.main;
-import mvnp2versionmatch.main.*;
-
+package mavenp2versionmatch.main;
 import junit.framework.TestCase;
 
 public class CommandTest extends TestCase{
 	public CommandTest(String name){
 		super(name);
 	}
-
+	
 	public void testCommands() throws Exception {
-		mvnp2versionmatch.main.Command ca = Command.findByStr("add");
-		assertEquals(ca, Command.ADD);
-//		assertEquals("add", Command.findByStr("add"));
-//		assertEquals("find", Command.findByStr("find"));
-//		assertEquals("update", Command.findByStr("update"));
-//		assertEquals(null, Command.findByStr("notacommand"));
+		assertEquals(Command.ADD, Command.findByStr("add"));
+		assertEquals(Command.FIND, Command.findByStr("find"));
+		assertEquals(Command.UPDATE, Command.findByStr("update"));
+		assertEquals(null, Command.findByStr("notacommand"));
+		assertEquals(null, Command.findByStr(""));
 	}
 
 }
