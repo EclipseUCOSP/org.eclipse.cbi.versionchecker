ed0a0bea379bbb59be210457450651c13a63b766
Mike Lim <mike@mjlim.net>
2012-11-15 01:18:21 -0800

    add DBIException and modify interface to use it. changes required in classes implementing DBI before this will build.

--- src/main/mavenp2versionmatch/db/DBI.java
+++ src/main/mavenp2versionmatch/db/DBI.java
@@ -5,13 +5,14 @@ import mavenp2versionmatch.main.VersionManifest;
 import java.sql.*;
 import java.util.List;
 import java.util.Map;
+import mavenp2versionmatch.exception.DBIException;
 
 public interface DBI {
-	public void open() throws SQLException;
-	public void close() throws SQLException;
-	public void addRecord(Map<String, String> colMap) throws SQLException;
-	public void updateRecord(Map<String, String> matchMap, Map<String, String> updateMap) throws SQLException;
-	public List<VersionManifest> find(Map<String, String> map) throws SQLException;
-	public List<VersionManifest> findAll() throws SQLException;
+	public void open() throws DBIException;
+	public void close() throws DBIException;
+	public void addRecord(Map<String, String> colMap) throws DBIException;
+	public void updateRecord(Map<String, String> matchMap, Map<String, String> updateMap) throws DBIException;
+	public List<VersionManifest> find(Map<String, String> map) throws DBIException;
+	public List<VersionManifest> findAll() throws DBIException;
 }
 
---
+++ src/main/mavenp2versionmatch/exception/DBIException.java
@@ -0,0 +1,21 @@
+package mavenp2versionmatch.exception;
+
+public class DBIException extends Exception {
+
+	public DBIException() {
+		super();
+	}
+
+	public DBIException(String message) {
+		super(message);
+	}
+
+	public DBIException(Throwable cause) {
+		super(cause);
+	}
+
+	public DBIException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
