3e8de7c9e9d3af22b9494066e3c7875e63aa21bf
Dave Hughes <dave.hughes@elasticpath.com>
2013-03-26 23:21:28 -0700

    Only prompt for credentials when the transport fails.

--- org.eclipse.cbi.versionchecker.ui/src/org/eclipse/cbi/versionchecker/ui/actions/VCCloneTask.java
+++ org.eclipse.cbi.versionchecker.ui/src/org/eclipse/cbi/versionchecker/ui/actions/VCCloneTask.java
@@ -13,13 +13,16 @@ import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.CreateBranchCommand.SetupUpstreamMode;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ResetCommand.ResetType;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.storage.file.FileRepository;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 /**
- * VCCloneTask represent a life cycle of a git clone action. Including database
- * query for gitRepo and gitBranch, getting user name and password from a UI
- * dialog, actual git operation and error report.
+ * VCCloneTask represents the lifecycle of a git clone action. Includes the database
+ * query for gitRepo and gitBranch, retrieval of username and password using a UI
+ * dialog, and git execution and error reporting.
  */
 public class VCCloneTask {
 	private String id;
@@ -78,46 +81,55 @@ public class VCCloneTask {
 		JFrame f = new JFrame();
 		f.setSize(d);
 		f.setResizable(false);
-		VCGitWindow vcgw = new VCGitWindow(f);
-		vcgw.show();
 
-		if (vcgw.status()) {
-			JFileChooser jfc = new JFileChooser();
-			jfc.setDialogTitle("Select Destination Folder");
-			jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-			int ret = jfc.showOpenDialog(f);
+		JFileChooser jfc = new JFileChooser();
+		jfc.setDialogTitle("Select Destination Folder");
+		jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int ret = jfc.showOpenDialog(f);
 
-			if (ret == JFileChooser.APPROVE_OPTION) {
-				File loc = jfc.getSelectedFile();
-				try {
-					CloneCommand clone = Git.cloneRepository();
-					clone.setBare(false);
-					
-					clone.setDirectory(loc).setURI(this.gitRepo).setCloneAllBranches(true);
-					
-					UsernamePasswordCredentialsProvider user = new UsernamePasswordCredentialsProvider(
-							vcgw.getLogin(), vcgw.getPass());
-					clone.setCredentialsProvider(user);
-					clone.call();
-					
-					FileRepository localRepo = new FileRepository(loc + "/.git");
-					Git git = new Git(localRepo);
-					
-					git.checkout().setCreateBranch(true)
-						.setName(gitBranch)
-						.setUpstreamMode(SetupUpstreamMode.SET_UPSTREAM)
-						.setStartPoint("origin/" + gitBranch).call();
-					
-					if (!this.latestFlag) {
-						git.reset().setMode(ResetType.HARD).setRef(gitCommit).call();
-					}
-				} catch (Exception e) {
-					JOptionPane.showMessageDialog(null, "Clone failed: " + e.getMessage(), "VersionChecker", 1);
-					return;
-				}
+		if (ret == JFileChooser.APPROVE_OPTION) {
+			File loc = jfc.getSelectedFile();
+			try {
+				executeCloneCommand(f, loc);
 				
-				JOptionPane.showMessageDialog(null, "Clone successfully","VersionChecker", 1);
+				FileRepository localRepo = new FileRepository(loc + "/.git");
+				Git git = new Git(localRepo);
+				
+				git.checkout().setCreateBranch(true)
+					.setName(gitBranch)
+					.setUpstreamMode(SetupUpstreamMode.SET_UPSTREAM)
+					.setStartPoint("origin/" + gitBranch).call();
+				
+				if (!this.latestFlag) {
+					git.reset().setMode(ResetType.HARD).setRef(gitCommit).call();
+				}
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(null, "Clone failed: " + e.getMessage(), "VersionChecker", 1);
+				return;
+			}
+			
+			JOptionPane.showMessageDialog(null, "Clone successfully","VersionChecker", 1);
 
+		}
+	}
+
+	private void executeCloneCommand(JFrame parentFrame, File loc) throws InvalidRemoteException, TransportException, GitAPIException {
+		CloneCommand clone = Git.cloneRepository();
+		clone.setBare(false);
+		
+		clone.setDirectory(loc).setURI(this.gitRepo).setCloneAllBranches(true);
+		
+		try {
+			clone.call();
+		} catch (TransportException e) {
+			VCGitWindow vcgw = new VCGitWindow(parentFrame);
+			vcgw.show();
+
+			if (vcgw.status()) {
+				UsernamePasswordCredentialsProvider user = new UsernamePasswordCredentialsProvider(
+						vcgw.getLogin(), vcgw.getPass());
+				clone.setCredentialsProvider(user);
+				clone.call();
 			}
 		}
 	}
